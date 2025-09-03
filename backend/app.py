82bebbc43ead49277e5bc51b778570927eecfc03
joaquin <joaquin@debian.joaquin>
2025-09-03 11:42:35 -0300

    version 0.1 del backend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,12 @@
 #   PUNTO DE ENTRADA 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hola Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
